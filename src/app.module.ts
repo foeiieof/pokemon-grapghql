2983cf0e648c8862569d025de2e2f65f19a9a275
foeiieof <akarawat.chant@gmail.com>
2025-02-16 19:52:27 +0700

    update : playground#5

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,7 +10,7 @@ dotenv.config();
     driver: ApolloDriver, autoSchemaFile: "schema.gql",
     playground: true,
     debug: true,
-    path: '/'
+    path: '/graphql'
   }),
     PokemonsModule
   ],
