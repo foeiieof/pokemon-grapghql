6485e4677a712cf7eee33f1f6842dbaa96f6e67f
foeiieof <akarawat.chant@gmail.com>
2025-02-16 20:30:05 +0700

    update : playground#11

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,22 @@
+
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { PokemonsModule } from './interface/graphql/pokemons.module';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
 
 @Module({
   imports: [
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
-      autoSchemaFile: 'schema.gql', playground: true, debug: true,
+      autoSchemaFile: 'schema.gql',
+      playground: true,
+      debug: true,
       path: '/graphql',
     }),
-    PokemonsModule,],
+    PokemonsModule,
+  ],
 })
 export class AppModule { }
