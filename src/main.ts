1fdfaceb7c94b0d328f8c4a0bd4bf67e3bbc47b2
foeiieof <akarawat.chant@gmail.com>
2025-02-16 20:20:28 +0700

    update : playground#8

--- src/main.ts
+++ src/main.ts
@@ -2,23 +2,12 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as express from 'express';
 import * as path from 'path';
-import { buildSchema } from 'graphql';
+
 const PORT = process.env.PORT || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.use('/graphql', express.static(path.join(__dirname, 'public')));
-
   app.use('/graphiql', express.static(path.join(__dirname, 'public')));
-
-  app.use('/graphql', express.json(), (req, res, next) => {
-    const graphql = require('express-graphql');
-    const schema = buildSchema('schema.gql'); return graphql({
-      schema,
-      graphiql: true,
-    })(req, res, next);
-  });
-
   await app.listen(PORT);
   console.log(`App running on: ${await app.getUrl()}`);
 }
